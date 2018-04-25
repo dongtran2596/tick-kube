e54ac65421c52f95ca543f757533cf07d38d31ba
Trần Văn Đông <ghostdragon2596@gmail.com>
2018-04-26 00:52:28 +0700

    Update create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -6,21 +6,21 @@ kube() {
 create() {
   echo "Creating tick..."
   echo "tick is the full stack of InfluxData products running in production configuration"
-  kube $BP/namespace.yaml # Tạo namespace cho tick
+  kube $HOME/tick-kube/namespace.yaml # Tạo namespace cho tick
   # Tạo config map đến telegraf.conf và influxdb.conf
-  kubectl create configmap --namespace tick telegraf-config --from-file $BP/telegraf/telegraf.conf 
-  kubectl create configmap --namespace tick influxdb-config --from-file $BP/influxdb/influxdb.conf
+  kubectl create configmap --namespace tick telegraf-config --from-file $HOME/tick-kube/telegraf/telegraf.conf 
+  kubectl create configmap --namespace tick influxdb-config --from-file $HOME/tick-kube/influxdb/influxdb.conf
   # Chạy influxdb deployment và service
-  kube $BP/influxdb/deployment.yaml
-  kube $BP/influxdb/service.yaml
+  kube $HOME/tick-kube/influxdb/deployment.yaml
+  kube $HOME/tick-kube/influxdb/service.yaml
   # Chạy kapacitor deployment và service
-  kube $BP/kapacitor/deployment.yaml
-  kube $BP/kapacitor/service.yaml
+  kube $HOME/tick-kube/kapacitor/deployment.yaml
+  kube $HOME/tick-kube/kapacitor/service.yaml
   # Chạy telegraf daemonset
-  kube $BP/telegraf/daemonset.yaml
+  kube $HOME/tick-kube/telegraf/daemonset.yaml
   # Chạy chornonograf deployment và service
-  kube $BP/chronograf/deployment.yaml
-  kube $BP/chronograf/service.yaml
+  kube $HOME/tick-kube/chronograf/deployment.yaml
+  kube $HOME/tick-kube/chronograf/service.yaml
   echo "Waiting for running pods..."
   kubectl get pods --namespace tick -w
 }
